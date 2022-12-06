b6cb4c7896f5baa0fa7f62f21cd40b9dbeab5abb
Abraham Meza <abraham.meza@sciodesk.com>
2022-12-06 12:05:07 -0600

    ADD UnitTest project in net6

---
+++ Multicomp.Oscilloscope/Multicomp.Oscilloscope.Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+using Multicomp.Oscilloscope.Enums;
+
+namespace Multicomp.Oscilloscope.Test;
+
+public class UnitTest1
+{
+    private async Task ConfigOscilloscope(Oscilloscope oscilloscope)
+    {
+        await oscilloscope.Reset();
+        await oscilloscope.SetChannelStatus(Source.CHAN2, Status.OFF);
+        await oscilloscope.SetChannelCoupling(Source.CHAN1, Coupling.DC);
+        await oscilloscope.SetChannelInverted(Source.CHAN1, Status.OFF);
+        await oscilloscope.SetChanelProbe(Source.CHAN1, "1X");
+        await oscilloscope.SetChannelScale(Source.CHAN1, "1V");
+        await oscilloscope.SetChannelOffset(Source.CHAN1, -4);
+        await oscilloscope.SetTimebaseScale("100ms");
+        await oscilloscope.SetAcquireMode(AcqMode.PEAK);
+        await oscilloscope.SetTriggerType();
+        await oscilloscope.SetTriggerSingleSweep(TriggerSingleSweep.NORM);
+        await oscilloscope.SetTriggerSingleEdgeLevel("7V");
+        await oscilloscope.Stop();
+        await oscilloscope.Run();
+        await oscilloscope.SetTriggerSingleSweep(TriggerSingleSweep.SING);
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        Oscilloscope oscilloscope = new("172.31.1.120", 3000);
+        await ConfigOscilloscope(oscilloscope);
+        var status = await oscilloscope.GetTriggerStatus();
+        status = await oscilloscope.GetRunState();
+    }
+}
