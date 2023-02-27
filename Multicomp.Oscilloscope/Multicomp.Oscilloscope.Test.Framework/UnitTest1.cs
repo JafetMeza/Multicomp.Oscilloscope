e3871293011d9571280c7214c723cd1c7316598f
Miguel Rosas <miguel.rosas@sciodesk.com>
2023-02-27 15:46:36 -0600

    Source was parsed to C# 10. Also another issues were fixed

--- Multicomp.Oscilloscope/Multicomp.Oscilloscope.Test.Framework/UnitTest1.cs
+++ Multicomp.Oscilloscope/Multicomp.Oscilloscope.Test.Framework/UnitTest1.cs
@@ -1,40 +1,36 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Multicomp.Oscilloscope.Enums;
-using System;
-using System.Threading.Tasks;
+﻿using Multicomp.Oscilloscope.Enums;
+using Xunit;
 
-namespace Multicomp.Oscilloscope.Test.Framework
+namespace Multicomp.Oscilloscope.Test.Framework;
+
+public class UnitTest1
 {
-    [TestClass]
-    public class UnitTest1
+
+    private async Task ConfigOscilloscope(Oscilloscope oscilloscope)
     {
+        await oscilloscope.Reset();
+        await oscilloscope.SetChannelStatus(Source.CHAN2, Status.OFF);
+        await oscilloscope.SetChannelCoupling(Source.CHAN1, Coupling.DC);
+        await oscilloscope.SetChannelInverted(Source.CHAN1, Status.OFF);
+        await oscilloscope.SetChanelProbe(Source.CHAN1, "1X");
+        await oscilloscope.SetChannelScale(Source.CHAN1, "1V");
+        await oscilloscope.SetChannelOffset(Source.CHAN1, -4);
+        await oscilloscope.SetTimebaseScale("100ms");
+        await oscilloscope.SetAcquireMode(AcqMode.PEAK);
+        await oscilloscope.SetTriggerType();
+        await oscilloscope.SetTriggerSingleSweep(TriggerSingleSweep.NORM);
+        await oscilloscope.SetTriggerSingleEdgeLevel("7V");
+        await oscilloscope.Stop();
+        await oscilloscope.Run();
+        await oscilloscope.SetTriggerSingleSweep(TriggerSingleSweep.SING);
+    }
 
-        private async Task ConfigOscilloscope(Oscilloscope oscilloscope)
-        {
-            await oscilloscope.Reset();
-            await oscilloscope.SetChannelStatus(Source.CHAN2, Status.OFF);
-            await oscilloscope.SetChannelCoupling(Source.CHAN1, Coupling.DC);
-            await oscilloscope.SetChannelInverted(Source.CHAN1, Status.OFF);
-            await oscilloscope.SetChanelProbe(Source.CHAN1, "1X");
-            await oscilloscope.SetChannelScale(Source.CHAN1, "1V");
-            await oscilloscope.SetChannelOffset(Source.CHAN1, -4);
-            await oscilloscope.SetTimebaseScale("100ms");
-            await oscilloscope.SetAcquireMode(AcqMode.PEAK);
-            await oscilloscope.SetTriggerType();
-            await oscilloscope.SetTriggerSingleSweep(TriggerSingleSweep.NORM);
-            await oscilloscope.SetTriggerSingleEdgeLevel("7V");
-            await oscilloscope.Stop();
-            await oscilloscope.Run();
-            await oscilloscope.SetTriggerSingleSweep(TriggerSingleSweep.SING);
-        }
-
-        [TestMethod]
-        public async Task TestMethod1()
-        {
-            Oscilloscope oscilloscope = new Oscilloscope("172.31.1.120", 3000);
-            await ConfigOscilloscope(oscilloscope);
-            var status = await oscilloscope.GetTriggerStatus();
-            status = await oscilloscope.GetRunState();
-        }
+    [Fact]
+    public async Task TestMethod1()
+    {
+        Oscilloscope oscilloscope = new Oscilloscope("172.31.1.120", 3000);
+        await ConfigOscilloscope(oscilloscope);
+        var status = await oscilloscope.GetTriggerStatus();
+        status = await oscilloscope.GetRunState();
     }
 }
--- Multicomp.Oscilloscope/Multicomp.Oscilloscope/Oscilloscope.cs
+++ Multicomp.Oscilloscope/Multicomp.Oscilloscope/Oscilloscope.cs
@@ -1,70 +1,72 @@
 ﻿using System;
 using System.IO;
-using System.Net.Sockets;
 using System.Text;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
-namespace Multicomp.Oscilloscope
+namespace Multicomp.Oscilloscope;
+
+public class Oscilloscope
 {
-    public class Oscilloscope
+    private readonly string _ip;
+    private readonly int _port;
+
+    public Oscilloscope(string ip, int port)
     {
-        private readonly string _ip;
-        private readonly int _port;
+        _ip = ip;
+        _port = port;
+    }
 
-        public Oscilloscope(string ip, int port)
+    public async Task WriteRawCommand(string command)
+    {
+        using (var client = new TcpClient())
         {
-            _ip = ip;
-            _port = port;
+            await client.ConnectAsync(_ip, _port);
+            using (var stream = client.GetStream())
+            using (var writeStream = new StreamWriter(stream))
+                writeStream.Write(Encoding.ASCII.GetBytes(command));
         }
+        await Task.Delay(450);
+    }
 
-        public async Task WriteRawCommand(string command)
+    public async Task<string> GetDataRawCommand(string command)
+    {
+        var counter = 0;
+        using var client = new TcpClient();
+        await client.ConnectAsync(_ip, _port);
+        using var stream = client.GetStream();
+        using var writeStream = new StreamWriter(stream);
+        using var readStream = new StreamReader(stream);
+        writeStream.Write(Encoding.ASCII.GetBytes(command));
+        bool repeat;
+        var bytes = 0;
+        var data = new byte[256];
+        do
         {
-            using (var client = new TcpClient())
+            repeat = false;
+            Array.Clear(data, 0, data.Length);
+            stream.ReadTimeout = 5;
+            if (counter >= 3) throw new Exception("Error de lectura");
+            try
             {
-                client.Connect(_ip, _port);
-                using (var stream = client.GetStream())
-                using (var writeStream = new StreamWriter(stream))
-                    writeStream.Write(Encoding.ASCII.GetBytes(command));
+                counter++;
+                bytes = stream.Read(data, 0, data.Length);
             }
-            await Task.Delay(450);
-        }
-
-        public async Task<string> GetDataRawCommand(string command)
-        {
-            int counter = 0;
-            using (var client = new TcpClient())
+            catch (Exception)
             {
-                client.Connect(_ip, _port);
-                using (var stream = client.GetStream())
-                {
-                    using (var writeStream = new StreamWriter(stream))
-                    {
-                        using (var readStream = new StreamReader(stream))
-                        {
-                            writeStream.Write(Encoding.ASCII.GetBytes(command));
-                        Read:
-                            var data = new byte[256];
-                            stream.ReadTimeout = 5;
-                            int bytes;
-                            if (counter >= 3) throw new Exception("Error de lectura");
-                            try
-                            {
-                                counter++;
-                                bytes = stream.Read(data, 0, data.Length);
-                            }
-                            catch (Exception)
-                            {
-                                await Task.Delay(400);
-                                writeStream.Write(Encoding.ASCII.GetBytes("1"));
-                                goto Read;
-                            }
-                            var response = Encoding.ASCII.GetString(data, 0, bytes);
-                            Disconnect();
-                            return response ?? "Sin respuesta";
-                        }
-                    }
-                }
+                await Task.Delay(400);
+                writeStream.Write(Encoding.ASCII.GetBytes("1"));
+                repeat = true;
             }
-        }
+        } while (repeat);
+
+        var response = Encoding.ASCII.GetString(data, 0, bytes);
+        Disconnect();
+        return string.IsNullOrEmpty(response) ? "Sin respuesta" : response;
+    }
+
+    public void Disconnect()
+    {
+        throw new NotImplementedException();
     }
 }
