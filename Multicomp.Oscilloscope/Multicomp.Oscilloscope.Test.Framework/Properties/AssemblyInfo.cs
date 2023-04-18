a857f922a2aa5160cc70e54a34c3f87b8b292255
Abraham Meza <abraham.meza@sciodesk.com>
2023-04-18 14:10:05 -0600

    ADD netFramework Test project

---
+++ Multicomp.Oscilloscope/Multicomp.Oscilloscope.Test.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Multicomp.Oscilloscope.Test.Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Multicomp.Oscilloscope.Test.Framework")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a81eb068-725e-4539-8ff9-0dbd2cb75b6e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
